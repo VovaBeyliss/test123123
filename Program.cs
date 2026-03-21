5811378095f0c4fe0a26b46c205d2f12de62a30b
VovaBeyliss <beylisvl@gmail.com>
2026-03-21 19:41:20 +0200

    test

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Linq_GropuBy_Practise {
+    public record Product(string Name, string Category, int Price);
+
+    class Program {
+        public static void Main(string[] args) {
+            var numbers = new List<int> { 12, 5, 8, 21, 44, 7, 3, 10, 15, 20 };
+
+            var groups = numbers.GroupBy(n => n % 2 == 0 ? "Even" : "Odd");
+
+            foreach (var group in groups) {
+                Console.WriteLine($"Catefory: {group.Key}: {group.Max()}");
+            }
+
+            /*
+            foreach (var group in groups)
+            {
+                Console.Write($"{group.Key}: ");
+
+                foreach (var item in group) {
+                    Console.Write($"{item}; ");
+                }
+
+                Console.WriteLine();
+            }
+            */
+        }
+    }
+}
